dc976c015645ca844b7939d3cadc780bdab7d771
Sam Backus <sbackus@patientslikeme.com>
2016-09-17 15:32:13 -0400

    added second instance of angry plant

--- game.rb
+++ game.rb
@@ -12,6 +12,7 @@ class MyWindow < Gosu::Window
     super(WIDTH, HEIGHT)
     self.caption = 'Make the plant happy!'
     @angry_plant = AngryPlant.new
+    @angry_plant2 = AngryPlant.new
     @water = Water.new
     @grass = Grass.new
     @squishiness_factor = 0.7
@@ -23,6 +24,7 @@ class MyWindow < Gosu::Window
       @water.draw
     end
     @angry_plant.draw
+    @angry_plant2.draw
   end
 
   def update
@@ -30,11 +32,15 @@ class MyWindow < Gosu::Window
       @angry_plant.make_happy
     end
     @angry_plant.update
+    if are_colliding?(@angry_plant2, @water)
+      @angry_plant2.make_happy
+    end
+    @angry_plant2.update
     @water.update
   end
 
   def are_colliding?(object1, object2)
-    Gosu::distance(object1.center_x, object1.center_y, object2.center_x, @water.center_y) < (@water.radius + object1.radius) * @squishiness_factor 
+    Gosu::distance(object1.center_x, object1.center_y, object2.center_x, @water.center_y) < (@water.radius + object1.radius) * @squishiness_factor
   end
 end
 
